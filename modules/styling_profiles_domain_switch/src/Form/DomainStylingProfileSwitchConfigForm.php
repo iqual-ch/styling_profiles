b6d016f6d3928a6773253dd9dfd14d58905b4af8
Guillaume Pacilly <guillaume.pacilly@gmail.com>
2026-01-21 11:49:41 +0000

    DUP-291: fix wrong ConfigForm instantiation

--- modules/styling_profiles_domain_switch/src/Form/DomainStylingProfileSwitchConfigForm.php
+++ modules/styling_profiles_domain_switch/src/Form/DomainStylingProfileSwitchConfigForm.php
@@ -23,32 +23,13 @@ class DomainStylingProfileSwitchConfigForm extends ConfigFormBase {
   protected $entityTypeManager;
 
   /**
-   * Constructs a DomainStylingProfileSwitchConfigForm object.
-   *
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The factory for configuration objects.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   */
-  public function __construct(ConfigFactoryInterface $config_factory, EntityTypeManagerInterface $entity_type_manager) {
-    parent::__construct($config_factory);
-    $this->entityTypeManager = $entity_type_manager;
-  }
-
-  /**
-   * Create function return static domain loader configuration.
-   *
-   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
-   *   Load the ContainerInterface.
-   *
-   * @return static
-   *   return domain loader configuration.
+   * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
-    return new static(
-        $container->get('config.factory'),
-        $container->get('entity_type.manager')
-    );
+    $instance = parent::create($container);
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+
+    return $instance;
   }
 
   /**
